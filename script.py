f4a3c371621abf1a52814c9a9b5b45f336f40f37
Jay Patel <pjay20301@gmail.com>
2022-11-22 11:56:12 +0530

    sessions recorded

--- script.py
+++ script.py
@@ -1,16 +1,28 @@
 import json
 
-f = open('HCI.side')
+f = open('newirctc.side')
 
 data = json.load(f)
 
 numClicks = 0
-
+numMouseOver = 0
+numMouseout = 0
+numSearch = 0
 for i in data['tests']:
     for j in i['commands']:
             if(j['command'] == 'click'):
                 numClicks += 1
+                if((".primaryBtn" in j['target']) or (".train_Search" in j['target']) or ("BE_train_search_btn" in j['target'])):
+                    numSearch += 1
+            elif(j['command'] == 'mouseOver'):
+                numMouseOver += 1   
+            elif(j['command'] == 'mouseOut'):
+                numMouseout += 1
+            
 
 print(numClicks)
+print(numMouseOver)
+print(numMouseout)
+print(numSearch)
 
 f.close()
